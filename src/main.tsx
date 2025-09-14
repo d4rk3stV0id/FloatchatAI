89321965d4c52ddca869088e9168e1c512618570
dhanush4u2 <153162556+dhanush4u2@users.noreply.github.com>
2025-09-15 00:52:29 +0530

    Map changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import 'leaflet/dist/leaflet.css'; // <-- ADD THIS LINE
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-velocity/dist/leaflet-velocity.css';
 
 createRoot(document.getElementById("root")!).render(<App />);
